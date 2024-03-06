cbe1b57730b2f81c96270bb5f1442478d0e66c3d
Ricardo Subtil <ricasubtil@gmail.com>
2024-03-06 14:46:37 +0000

    Add build support for macOS (arm64)

---
+++ images/base-macosarm64/toolchain.cmake
@@ -0,0 +1,11 @@
+set(CMAKE_SYSTEM_NAME Darwin)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+
+set(triple aarch64-apple-darwin21.4)
+
+set(CMAKE_C_COMPILER ${triple}-clang)
+set(CMAKE_CXX_COMPILER ${triple}-clang++)
+set(CMAKE_RANLIB ${triple}-ranlib)
+set(CMAKE_AR ${triple}-ar)
+
+set(CMAKE_OSX_DEPLOYMENT_TARGET 12.3)
